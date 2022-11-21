0924621f9fa769e7a8124fa88b628fb2199a8e94
xzendercage <akramalikhan459@gmail.com>
2022-11-22 00:32:15 +0530

    TiTan Music

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center"><img src="./assets/logo.gif" width="30px"> TiTan Music Bot <img src="./assets/logo.gif" width="30px"></h1>
+> Note: Lavalink not needed for music functionality :)
+
+# TiTan 𝄞
+### 💨 Run the project's
+Make sure to install node16 +
+Glitch: [![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/xzendercage/titan)
+
+Repl: [![Run on Repl.it](https://repl.it/badge/github/xzendercage/titan)](https://replit.com/@xzndercage/titan)
+
+ 
+## 📝 [Support Server](https://discord.gg/VrZ4vwFW)
+
+If you have major coding issues with this bot, please join and ask for help.
+
+> Note: If you are hosting your bot in heroku, Please consider upgrading your dyno for running dashboard & bot simultaneously because in free dyno it'll run out of memory(as there are two workers). If you want to run only the bot, turn off the `web` dyno.
+
+[![Discord Support](https://discord.com/api/guilds/1025372470208905236/widget.png?style=banner2)](https://discord.gg/JNbspXYX)
+
+Made with :heart: and JavaScript!
---
+++ commands/info/help.js
@@ -0,0 +1,69 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+
+  name: "help",
+
+  description:
+
+    "Get list of all command and even get to know every command detials",
+
+  usage: "help <cmd>",
+
+  category: "info",
+
+  run: async (client, message, args) => {
+
+    if (args[0]) {
+
+      const command = await client.commands.get(args[0]);
+
+      if (!command) {
+
+        return message.channel.send("Unknown Command: " + args[0]);
+
+      }
+
+      let embed = new MessageEmbed()
+
+        .setAuthor(command.name, client.user.displayAvatarURL())
+
+        .addField("❯ Description", command.description || "Not Provided :(")
+
+        .addField("❯ Usage", "`" + command.usage + "`" || "Not Provied")
+
+        .setThumbnail(client.user.displayAvatarURL())
+
+        .setColor("BLUE")
+
+        .setFooter(client.user.username, client.user.displayAvatarURL());
+
+      return message.channel.send(embed);
+
+    } else {
+
+      const embed1 = new MessageEmbed()
+        .setColor('#85b0d2')
+        .setTitle('Music Commands')
+        .addField('join', ` Join the voice channel!`, true)
+        .addField('leave', `Leaves the voice channel!`, true)
+        .addField('play', `Play the requested song in the vc`, true)
+        .addField('stop', `Clears the queue and leave the vc'`, true)
+        .addField('pause', ` Pause the queue`, true)
+        .addField('resume', ` Resume the queue`, true)
+        .addField('shuffle', ` Shuffle the queue`, true)
+        .addField('queue', ` Gives you info about the server queue`, true)
+        .addField('remove', ` Remove a song from the queue`, true)
+        .addField('loop', `Loop the queue`, true)
+        .addField('disable-loop', ` Stop looping the queue`, true)
+        .addField('skip', ` Skip the song that its playing.`, true)
+        .addField('clear', ` Clear the queue`, true)
+        .addField('playlist', `Play a playlist in the vc`, true)
+        .addField('volume', `Set the volume of the bot in the vc`, true)
+        .addField('nowplaying', `Gives info about the song that its being played and the progress of it`, true)
+        .setTimestamp()
+        .setFooter(`TiTan`)
+        message.channel.send(embed1)
+    }
+}
+}
